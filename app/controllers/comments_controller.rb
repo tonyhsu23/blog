e977a7b50583c69563779546a40b4c5833212bc7
Tony Hsu <t153426789@gmail.com>
2015-11-10 16:36:56 +0800

    add delete features to post and comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,6 +12,20 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+
+    respond_to do |format|
+      if @comment.destroy
+        format.js
+      else
+        flash[:alert] = "error"
+      end
+    end
+    # redirect_to root_path
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:response, :respondent)
