4f462c29625ef23e614f47b038376c6789765141
Tony Hsu <t153426789@gmail.com>
2015-11-10 18:19:56 +0800

    simplify comments methods

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,28 +2,13 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.new(comment_params)
-
-    respond_to do |format|
-      if @comment.save
-        format.js
-      else
-        flash[:alert] = "error"
-      end
-    end
+    @comment.save
   end
 
   def destroy
     @post = Post.find(params[:post_id])
     @comment = @post.comments.find(params[:id])
-
-    respond_to do |format|
-      if @comment.destroy
-        format.js
-      else
-        flash[:alert] = "error"
-      end
-    end
-    # redirect_to root_path
+    @comment.destroy
   end
 
   private
