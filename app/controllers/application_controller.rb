cee7c22222d052c97264992fc8adfab6d3f3916d
Tony Hsu <t153426789@gmail.com>
2015-12-02 23:07:45 +0800

    add authorization to delete post function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  helper_method :current_user
+  helper_method :current_user, :is_author
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
@@ -11,5 +11,10 @@ class ApplicationController < ActionController::Base
 
   def require_user
     redirect_to login_path unless current_user  
+  end
+
+  def is_author(id)
+    return false unless session[:user_id]
+    current_user == Post.find(id).user
   end
 end
