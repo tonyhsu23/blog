a7316406e66a711c4d1b4da1f77fb80466e41c9e
Tony Hsu <t153426789@gmail.com>
2015-12-14 22:48:18 +0800

    move header into partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+  def set_header_image  
+    controller = params[:controller]
+    action = params[:action]  
+    if controller == 'users' || controller == 'sessions'
+      '/assets/contact-bg.jpg'
+    elsif controller == 'posts' && (action == 'index' || action == 'new')
+      '/assets/home-bg.jpg'
+    elsif controller == 'abouts'
+      '/assets/about-bg.jpg'
+    else
+      nil
+    end
+  end
+
+  def set_header_title
+    (params[:controller] == 'abouts') ? "Tony Hsu" : "Tony's Blog"
+  end
+
+  def set_header_content
+    (params[:controller] == 'abouts') ? "My name is Tony Hsu." : "A Clean Blog Theme by Start Bootstrap"
+  end 
 end
